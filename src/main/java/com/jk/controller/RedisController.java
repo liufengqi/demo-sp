df541030d3a42043564d727d3408993558851464
zhangxinghua <zxh1511704850@outlook.com>
2018-05-14 15:44:40 +0800

    说过话

--- src/main/java/com/jk/controller/RedisController.java
+++ src/main/java/com/jk/controller/RedisController.java
@@ -3,7 +3,7 @@ package com.jk.controller;
 public class RedisController {
 
     public static void main(){
-        System.err.println("aaaaaa");
+        System.err.println("aaaaaa"+"bbbbbbbbbbbb");
     }
 
 }
